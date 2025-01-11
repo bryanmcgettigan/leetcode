61e51c4e213c4179cc9a4238adbfb74bdde14ac3
Bryan <bmcgettigan@DESKTOP-I4OTH5J>
2025-01-11 19:09:25 +0100

    121 improved answer

--- 121_best_time_stock/main.py
+++ 121_best_time_stock/main.py
@@ -3,16 +3,19 @@ def maxProfit(prices):
     :type prices: List[int]
     :rtype: int
     """
-    baseProfit =0
-    for index,num in enumerate(prices):
-            for numtwo in prices[index+1:]:
-                  profit=numtwo-num
-                  if(profit>baseProfit):
-                        baseProfit=profit
-
-    return baseProfit
+    hash_table = {}
+    maxProfit =0
+    cheapestPrice=max(prices)
+    for index,num in enumerate(prices[:-1]):
+        if(index>=0 and num<cheapestPrice):
+            cheapestPrice=num
+        if(prices[index+1]-cheapestPrice>maxProfit):
+            maxProfit=prices[index+1]-cheapestPrice
         
 
+
+    return maxProfit
+        
 prices = [7,1,5,3,6,4]
 
 print(maxProfit(prices))
