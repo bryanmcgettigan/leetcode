e85f22dc02671710301c2871ca82fef01abc3054
Bryan <bmcgettigan@DESKTOP-I4OTH5J>
2025-01-24 11:51:53 +0100

    383 solution hash map

---
+++ 383_ransom_note/main.py
@@ -0,0 +1,26 @@
+def canConstruct(ransomNote, magazine):
+    """
+    :type ransomNote: str
+    :type magazine: str
+    :rtype: bool
+    """
+    hmap_mag = {}
+    for char in magazine:
+        hmap_mag[char] = hmap_mag.get(char,0)+1
+    print(hmap_mag)
+
+    for char in ransomNote:
+        if hmap_mag.get(char):
+            hmap_mag[char] = hmap_mag.get(char)-1
+            if hmap_mag[char] <0:
+                return False
+        else:
+            return False
+        
+    return True
+
+
+ransomNote="abcdxe"
+magazine = "aabbcccdde"
+
+print(canConstruct(ransomNote,magazine))
